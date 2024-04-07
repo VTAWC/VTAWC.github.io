9f0d3c669b533c22cc16576e3996fc5b3767b306
trav-cc <tchan89@vt.edu>
2024-04-07 15:18:15 -0400

    Updated website with FAQ and officer board page

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function openNav() {
 function closeNav() {
   document.getElementById("side-nav").style.width = "0";
 }
+
+var q = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < q.length; i++) {
+  q[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
